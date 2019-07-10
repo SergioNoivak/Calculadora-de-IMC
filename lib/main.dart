296304909d95fcd8d4755fa66d2fbad3550ff685
Sergio Souza Novak <serginhosnovak@hotmail.com>
2019-07-10 10:41:44 -0300

    feito o aplicativo e resolvido problema da validação

--- lib/main.dart
+++ lib/main.dart
@@ -21,16 +21,45 @@ TextEditingController controladorKg = TextEditingController();
 TextEditingController controladorCm = TextEditingController();
 String textoInformativo = "";
 
+GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+
 void resetar(){
 
-
+  
   this.controladorCm.text = "";
   this.controladorKg.text = "";
+setState(() {
   this.textoInformativo="informe seus dados";
+  this._formKey = GlobalKey<FormState>();
+});
 
 }
 
+void calcular(){
+setState(() {
 
+
+  double weight = double.parse(this.controladorKg.text);
+  double height = double.parse(this.controladorCm.text)/100;
+  double imc = weight/(height*height);
+  
+  if(imc<18.6)
+    this.textoInformativo = "Abaixo do peso imc = ${imc.toStringAsPrecision(4)}";
+  else if(imc>= 18.6 && imc<24.9)
+    this.textoInformativo = "ok, peso ideal imc = ${imc.toStringAsPrecision(4)}";
+  else if(imc>= 24.9 && imc<29.9)
+    this.textoInformativo = "Levemente acima do peso  imc = ${imc.toStringAsPrecision(4)}";
+  else if(imc>= 29.9 && imc<34.9)
+    this.textoInformativo = "Obesidade grau 1 imc = ${imc.toStringAsPrecision(4)}";
+  else if(imc>= 34.9 && imc<39.9)
+    this.textoInformativo = "Obesidade grau 2 imc = ${imc.toStringAsPrecision(4)}";
+  else if(imc>= 40 )
+    this.textoInformativo = "Obesidade grau 3 imc = ${imc.toStringAsPrecision(4)}";
+      
+});
+
+}
 
 @override
 Widget build(BuildContext context) {
@@ -47,8 +76,10 @@ return Scaffold(
           ],
         ),
         backgroundColor: Colors.white,
-        body:SingleChildScrollView(child: 
-         Column(
+        body:SingleChildScrollView(
+          child:Form(
+              key: this._formKey,
+              child: Column(
 
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
@@ -57,7 +88,7 @@ return Scaffold(
                   size: 100,
                   color: Colors.green
                   ),
-                  TextField(keyboardType: TextInputType.number,
+                  TextFormField(keyboardType: TextInputType.number,
                             decoration: InputDecoration(
                                 
                                 labelText: "Altura (cm)",
@@ -66,8 +97,15 @@ return Scaffold(
                             
                          textAlign: TextAlign.center,
                           controller: controladorCm,     
+                          validator: (value){
+                            if(value.isEmpty)
+                              return "insira sua altura";
+
+
+
+                          },
                   ),
-                                TextField(keyboardType: TextInputType.number,
+                    TextFormField(keyboardType: TextInputType.number,
                             decoration: InputDecoration(
                                 
                                 labelText: "Peso (kg)",
@@ -75,11 +113,21 @@ return Scaffold(
                             ),
                             
                          textAlign: TextAlign.center,
-                        controller: controladorKg
+                        controller: controladorKg,
+                         validator: (value){
+                            if(value.isEmpty)
+                              return "insira seu Peso";
+                          }
                   ),
                   
                   Container(child:RaisedButton(
-                    onPressed: (){},
+                    onPressed:(){
+
+                      if(this._formKey.currentState.validate())
+                        calcular();
+
+
+                    } ,
                     child: Text("Calcular",style: TextStyle(color: Colors.white,fontSize: 15.0)),
                     color: Colors.green),
                     height: 50.0,),
@@ -91,6 +139,10 @@ return Scaffold(
 
         ),
         
+
+
+          )
+
         
         )
 
